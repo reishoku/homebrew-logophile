571a9b286a617c1019f004bab9d783606632d3ee
reishoku <36192030+reishoku@users.noreply.github.com>
2018-06-24 23:10:20 +0900

    Update logophile.rb

--- logophile.rb
+++ logophile.rb
@@ -1,9 +1,12 @@
-class 'logophile' < Cask
+cask 'logophile' do
   version :latest
   sha256 :no_check
 
   url 'http://dicwizard.jp/logophile/bin/Logophile.dmg'
+  name 'logophile'
   homepage 'http://dicwizard.jp/logophile'
+
+  auto_updates false
 
   app 'Logophile/Logophile.app'
   app 'Logophile/LogophileDicManager.app'
